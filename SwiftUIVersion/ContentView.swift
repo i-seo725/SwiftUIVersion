eb859a9f12924234b5f742070a801c5b4d67e3c8
i-seo725 <hjing719@gmail.com>
2023-11-23 10:11:13 +0900

    Initial Commit

---
+++ SwiftUIVersion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIVersion
+//
+//  Created by 이은서 on 11/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIVersion/SwiftUIVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIVersionApp.swift
+//  SwiftUIVersion
+//
+//  Created by 이은서 on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
